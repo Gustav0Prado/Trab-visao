9b317044c5cab438652c41ff9b0871ed335e169f
gustavops <guprado.silva@gmail.com>
2025-05-24 14:48:37 -0300

    [add] Classificador linear

--- T4/mnist-linear.py
+++ T4/mnist-linear.py
@@ -2,7 +2,7 @@
 
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import SGDClassifier 
 from sklearn.metrics import classification_report, accuracy_score
 import argparse
 
@@ -10,16 +10,12 @@ parser = argparse.ArgumentParser(
                     prog='Programa de testes usando MNIST',
                     description='Realiza teste no dataset MNIST usando os parametros passados')
 
-parser.add_argument('kneighbours')
-parser.add_argument('metric')
 parser.add_argument('test_size')
 
 args = parser.parse_args()
 
 # Carrega varáveis da linha de comando
 TEST_SIZE = float(args.test_size)
-K = int(args.kneighbours)
-METRIC = int(args.metric)
 
 # Carregar o dataset MNIST
 mnist = load_digits()
@@ -31,11 +27,11 @@ X_temp, X_test, y_temp, y_test = train_test_split(X, y, test_size=TEST_SIZE, ran
 X_train, X_val, y_train, y_val = train_test_split(X_temp, y_temp, test_size=0.25, random_state=42)
 
 # Treina o classificador
-knn = KNeighborsClassifier(n_neighbors=K, p=METRIC)
-knn.fit(X_train, y_train)
+sgdc = SGDClassifier(random_state=42)
+sgdc.fit(X_train, y_train)
 
-y_val_pred = knn.predict(X_val)
-y_pred = knn.predict(X_test)
+y_val_pred = sgdc.predict(X_val)
+y_pred = sgdc.predict(X_test)
 
 print("Validação: ", accuracy_score(y_val, y_val_pred))
 print("Teste:     ", accuracy_score(y_test, y_pred))
